a6ebdea8a50d200c83b6cbbd21cbfcae1cc4d1e8
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2025-11-09 23:32:20 +0600

    csv

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,6 +34,7 @@ import Timeline from './components/Timeline/Timeline';
 import ExampleDashboardComponent from './components/HealthLogInterface/ExampleUsage';
 import PatientProfile from './components/PatientProfile/PatientProfile';
 import SharedProfile from './components/SharedProfile';
+import { default as UserDoctorProfile } from './components/User/DoctorProfile';
 
 
 const theme = createTheme({
@@ -437,6 +438,16 @@ function App() {
                     </PatientRoute>
                   }
                 />
+
+                <Route 
+                path = '/doctor-profile'
+                element = {
+                  <PatientRoute>
+                    <UserDoctorProfile />
+                  </PatientRoute>
+                }
+                />
+
 
                 <Route
                   path='/patient/profile'
